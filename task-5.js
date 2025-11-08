bf8cc9d2a371e78176c619eedaad7fb23eee593c
Nugraha Panca Wibisana <nugrahapancawibisana@gmail.com>
2025-11-08 22:02:21 +0700

    weekly 1 task 5

---
+++ task-5.js
@@ -0,0 +1,23 @@
+const number = 10;
+
+const promise = (number) => {
+  return new Promise((resolve, reject) => {    
+    setTimeout(() => {
+      if (number) {
+        resolve(number);
+      } else {
+        reject("Invalid")
+      }
+    }, 1000);
+  });
+};
+
+(async (number, callback) => {
+  try {
+    for (let i = 1; i <= number; i++) {
+      console.log(await callback(i));
+    }
+  } catch (error) {
+    console.log(error);
+  }
+})(number, promise);
