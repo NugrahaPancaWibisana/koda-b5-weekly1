07901d36ca747e40b2794b7d286757f1eb79f9a3
Nugraha Panca Wibisana <111733086+NugrahaPancaWibisana@users.noreply.github.com>
2025-11-10 10:16:02 +0700

    Delete task-3.js

--- task-3.js
+++
@@ -1,9 +0,0 @@
-const celciusToFahrenheit = (celcius) => {
-  if (typeof celcius === 'number') {
-    return celcius * 1.8 + 32;
-  } else {
-    return 'Invalid number';
-  }
-};
-
-console.log(celciusToFahrenheit(30));
